37e9cd539174a658eb18dbd07cdebdee67d109c2
Mischa <mschindowski@gmail.com>
2017-03-31 13:03:16 +0200

    implement table selection

--- lib/yaml_db/rake_tasks.rb
+++ lib/yaml_db/rake_tasks.rb
@@ -1,7 +1,7 @@
 module YamlDb
   module RakeTasks
-    def self.data_dump_task
-      SerializationHelper::Base.new(helper).dump(db_dump_data_file(helper.extension))
+    def self.data_dump_task(table)
+      SerializationHelper::Base.new(helper).dump(db_dump_data_file(helper.extension), table)
     end
 
     def self.data_dump_dir_task
